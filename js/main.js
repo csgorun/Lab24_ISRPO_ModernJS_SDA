359cbe7aa10c699b7fe2eb55f7c770d21ce6a10c
csgorun <dimitryi455@gmail.com>
2026-04-08 09:49:53 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -1,57 +1,108 @@
-// console.log("\n=== Деструктизация массивов ===");
-// const colors = ["red", "green", "blue"];
+// // console.log("\n=== Деструктизация массивов ===");
+// // const colors = ["red", "green", "blue"];
 
-// const color1 = colors[0];
-// const color2 = colors[1];
-// console.log(color1, color2);
+// // const color1 = colors[0];
+// // const color2 = colors[1];
+// // console.log(color1, color2);
 
-// const [firstColor, secondColor, thirdColor] = colors;
-// console.log(firstColor, secondColor, thirdColor);
+// // const [firstColor, secondColor, thirdColor] = colors;
+// // console.log(firstColor, secondColor, thirdColor);
 
-// const [primary, , tertiary] = colors;
-// console.log(primary, tertiary);
+// // const [primary, , tertiary] = colors;
+// // console.log(primary, tertiary);
 
-// const [c1, c2, c3, c4 = "yellow"] = colors;
-// console.log(c4);
+// // const [c1, c2, c3, c4 = "yellow"] = colors;
+// // console.log(c4);
 
-// console.log("Деструктуризация обьектов");
+// // console.log("Деструктуризация обьектов");
+// // const user = {
+// //     name: "Алиса",
+// //     age: 25,
+// //     city: "Москва",
+// // };
+
+// // const userName = user.name;
+// // const userAge = user.age;
+// // console.log(userName, userAge);
+
+// // const { name, age, city } = user;
+// // console.log(name, age, city);
+
+// // const { name: fullName, age: years } = user;
+// // console.log(fullName, years);
+
+// // const { name: personName, country = "Россия" } = user;
+// // console.log(personName, country);
+
+// console.log("Деструктизация в параметрах");
 // const user = {
 //     name: "Алиса",
 //     age: 25,
 //     city: "Москва",
 // };
 
-// const userName = user.name;
-// const userAge = user.age;
-// console.log(userName, userAge);
+// function printUserOld(user) {
+//     console.log(`Имя: ${user.name}`);
+//     console.log(`Возраст: ${user.age}`);
+//     console.log(`Город ${user.city}`);
+// }
 
-// const { name, age, city } = user;
-// console.log(name, age, city);
+// function printUser({ name, age, city }) {
+//     console.log(`Имя: ${name}`);
+//     console.log(`Возраст: ${age}`);
+//     console.log(`Город: ${city}`);
+// }
+// printUserOld(user);
+// console.log();
+// printUserOld(user);
 
-// const { name: fullName, age: years } = user;
-// console.log(fullName, years);
+// console.log("Spread  для массивов");
 
-// const { name: personName, country = "Россия" } = user;
-// console.log(personName, country);
+//  const arr1 = [1, 2, 3];
+//  const arr2 = [4, 5, 6];
 
-console.log("Деструктизация в параметрах");
-const user = {
-    name: "Алиса",
-    age: 25,
-    city: "Москва",
-};
+//  const combined = [...arr1, ...arr2];
+//  console.log("Обьединённый массив:", combined);
 
-function printUserOld(user) {
-    console.log(`Имя: ${user.name}`);
-    console.log(`Возраст: ${user.age}`);
-    console.log(`Город ${user.city}`);
-}
+//  const copy = [...arr1];
+//  console.log("Копия массива:", copy);
 
-function printUser({ name, age, city }) {
-    console.log(`Имя: ${name}`);
-    console.log(`Возраст: ${age}`);
-    console.log(`Город: ${city}`);
-}
-printUserOld(user);
-console.log();
-printUserOld(user);
+//  const extended = [0, ...arr1, 7, 8];
+//  console.log("Расширенный массив:", extended);
+
+//  console.log("Spread для обьектов");
+
+// const person = {
+//     name: "Иван",
+//     age: 30,
+// };
+
+// const address = {
+//     city: "Санкт-Петербург",
+//     street: "Невский проспект",
+// };
+
+// const fullInfo = { ...person, ...address};
+// console.log("полная информация:", fullInfo);
+
+// const personCopy = { ...person };
+// console.log("Копия обьекта:", personCopy);
+
+// const update = { ...person, age: 31, occupation: "Developer"};
+// console.log("обновленный объект:", update);
+
+console.log("Rest оператор");
+
+ function sum (...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+ }
+
+ console.log("Сумма 1,2,3:", sum(1, 2, 3));
+ console.log("Сумма 1,2,3,4,5:", sum(1, 2, 3, 4, 5));
+
+
+ const numbers = [10, 20, 30, 40, 50];
+ const [first, second, ...rest] = numbers;
+ console.log("Первое число:", first);
+ console.log("Второе число", second);
+ console.log("Остальные числа:", rest);
